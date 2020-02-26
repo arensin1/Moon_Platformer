36c8b6d5c8a14709e41476b7f8550e435aa1c9c6
Young Jie <young_jie_cho@brown.edu>
2020-02-26 14:17:55 +0000

    First commit

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public characterController controller;
+    public float runningSpeed = 40f;
+    float hori = 0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hori = Input.GetAxisRaw("Horizontal") * runningSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(hori * Time.fixedDeltaTime, false);
+    }
+}
