c49acbb519a678189f5131b1bd7459266a1e250f
arensin1 <alexrensing18@gmail.com>
2020-02-28 11:46:51 +0000

    camera

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing = 5f;
+    Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector3(0,0,-10);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 targetCam = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, targetCam,
+        smoothing * Time.deltaTime);
+    }
+}
