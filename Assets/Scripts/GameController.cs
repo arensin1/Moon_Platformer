a95e45b87d6062dc9b35a5ed920ecc9579c065c4
Young Jie <young_jie_cho@brown.edu>
2020-03-02 16:26:49 +0000

    timer and win lose situations

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text uiText;
+    public characterController player;
+    private float timeLeft;
+	public Text timeText;
+    bool timechange; // to check when we have to keep run the timer
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       uiText.text = "Goal: Find the Flower";
+	   timeText.text = "Time Left: ";
+       timeLeft = 40.0f;
+       timechange = true;
+    }
+    void FixedUpdate()
+    {
+        if (timechange){
+            timeLeft -= Time.deltaTime;
+		    timeText.text = "Time Left: " + timeLeft.ToString();
+            if (timeLeft<0){
+                timechange = false;
+                timeText.text = "00;00";
+                uiText.text = "Time's Up : (";
+                Color newColor = new Color(1,0,0,1);
+                uiText.color = newColor;
+                player.ourRigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
+            }
+        }
+    }
+    // win situation
+    public void winSituation()
+    {
+        if(player.obj_Complete){
+			uiText.text = "Good Job :)";
+            player.ourRigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
+            timechange = false;
+        }
+
+    }
+
+    //lose situation 1 : falling down
+    //lost situation 2 (on top of this code) : time's up
+    public void loseSituation()
+    {
+        if(player.ourRigidbody.position.y < -6){
+			uiText.text = "Aww you died :(";
+            Color newColor = new Color(1,0,0,1);
+            uiText.color = newColor;
+            player.ourRigidbody.constraints = RigidbodyConstraints2D.FreezePosition;
+            timechange = false;
+        }
+    }
+}
--- Assets/Scripts/characterController.cs
+++ Assets/Scripts/characterController.cs
@@ -4,21 +4,20 @@ using UnityEngine.Events;
 
 public class characterController : MonoBehaviour
 {
-     private Rigidbody2D ourRigidbody;
+     public Rigidbody2D ourRigidbody;
      [Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f; 
      private float m_JumpForce = 400f; 
      private bool m_Grounded = false;  
      private Vector3 m_Velocity = Vector3.zero;
      Collision2D collision;
-     bool obj_Complete = false;
+     public bool obj_Complete = false;
+     public GameController gameController;
    
 
 
     private void Awake()
     {
         ourRigidbody = GetComponent<Rigidbody2D>();
-
-        
 
     }
 
@@ -35,21 +34,21 @@ public class characterController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other){
             if (other.gameObject.CompareTag ("Flower"))
             {
-               
                 other.gameObject.SetActive(false);
                 obj_Complete = true;
             }
         }
     public void Move(float move, bool jump)
     {
-
+        //check the win and lose situation
+        gameController.winSituation();
+        gameController.loseSituation();
         
-            Vector3 targetVelocity = new Vector2(move * 10f, ourRigidbody.velocity.y);
+        Vector3 targetVelocity = new Vector2(move * 10f, ourRigidbody.velocity.y);
        
-            ourRigidbody.velocity = Vector3.SmoothDamp(ourRigidbody.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+        ourRigidbody.velocity = Vector3.SmoothDamp(ourRigidbody.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
      
-       
         // If the player should jump...
         if (m_Grounded && jump)
         {
