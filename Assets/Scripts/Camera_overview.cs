45311a0af036fa0f52bbba613dd1a49b4c73df4e
arensin1 <alexrensing18@gmail.com>
2020-04-18 00:11:00 -0500

    dialog/pause/cam

--- Assets/Scripts/Camera_overview.cs
+++ Assets/Scripts/Camera_overview.cs
@@ -7,6 +7,7 @@ public class Camera_overview : MonoBehaviour
     public float smoothing = 8f;
     private Vector3 velocity = Vector3.zero;
     public Vector3 position;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +17,11 @@ public class Camera_overview : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = Vector3.SmoothDamp(this.transform.position,position ,
+        if(!animator.GetBool("1stDialog"))
+        {
+            transform.position = Vector3.SmoothDamp(this.transform.position,position ,
         ref velocity, smoothing);
+        }
+        
     }
 }
