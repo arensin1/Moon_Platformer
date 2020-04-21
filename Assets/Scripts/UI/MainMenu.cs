215a0320f5b43ed91b196892d1470a5755820b23
Young Jie <young_jie_cho@brown.edu>
2020-04-22 01:03:16 +0900

    final version (almost)

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void playGame(){
+        //At start of game lives reset to 3
+        //sent to first level
+        DataHolder.Lives = 3;
+        SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex + 1);
+    }
+
+    public void credits(){
+        //credits
+        SceneManager.LoadScene(12);
+    }
+
+    public void goBackMenu(){
+        //go back to menu
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame ()
+    {
+        //quiting application
+        UnityEditor.EditorApplication.isPlaying = false; //for editor
+        Application.Quit(); //for built game
+    }
+}
